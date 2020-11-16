7558c413c4decc435d4e948e0fcb6c6a0d276605
anisssoudki <aniss.marrouch@gmail.com>
2020-11-16 15:37:53 -0700

    saying hello

---
+++ kitchen/src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// import {
+//     BrowserRouter,
+//     Router,
+//     Switch,
+//     Route
+//   } from "react-router-dom";
+// import RecipeList   from './recipes/RecipeList';
+// import RecipeShow   from './recipes/RecipeShow';
+// import RecipeEdit   from './recipes/RecipeEdit';
+// import RecipeDelete from './recipes/RecipeDelete';
+// import RecipeCreate from './recipes/RecipeCreate';
+// import Header from './Header';
+// import history from '../history'; 
+
+
+const App = () => {
+    return (
+<div>
+   
+  hello
+     
+</div>
+    ) 
+
+
+
+    
+}
+
+export default App
